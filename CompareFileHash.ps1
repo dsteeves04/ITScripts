c5bbf727d3f5b32e063811926491227a79d3b371
dsteeves04 <42791848+dsteeves04@users.noreply.github.com>
2019-03-05 11:25:18 -0500

    Add files via upload

---
+++ CompareFileHash.ps1
@@ -0,0 +1,17 @@
+﻿$fileA = "S:\Backups\FBBuild\FBBuildD2019-03-04T131708.vbk"
+
+$fileB = "E:\ArchivedVMs\2003 VMs\FBBuild\FBBuildD2019-03-04T131708.vbk"
+
+$hashA = Get-FileHash -Path $fileA -Algorithm MD5
+$hashB = Get-FileHash -Path $fileB -Algorithm MD5
+
+Write-Host '### Hash file A ###' -NoNewLine
+$hashA | Format-List
+Write-Host '### Hash file B ###' -NoNewline
+$hashB | Format-List
+
+if ($hashA.Hash -eq $hashB.Hash) {
+    Write-Host 'Get-FileHash results are consistent' -ForegroundColor Green
+} else {
+	Write-Host 'Get-FileHash results are inconsistent!!' -ForegroundColor Red
+}
