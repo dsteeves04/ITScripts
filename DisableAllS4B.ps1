ff0fd1d5cb4549d59b4a25471fac1bbdc72d47b0
dsteeves04 <42791848+dsteeves04@users.noreply.github.com>
2020-07-14 10:07:35 -0400

    Update DisableAllS4B.ps1

--- DisableAllS4B.ps1
+++ DisableAllS4B.ps1
@@ -2,5 +2,5 @@
 for($i = 0; $i -lt $AllLicensingPlans.Count; $i++)
 {
     $O365Licences = New-MsolLicenseOptions -AccountSkuId $AllLicensingPlans[$i].AccountSkuId -DisabledPlans "MCOSTANDARD"
-    Set-MsolUserLicense -UserPrincipalName Robin.Horner@docuware.com -LicenseOptions $O365Licences
+    Set-MsolUserLicense -UserPrincipalName First.Last@example.com -LicenseOptions $O365Licences
 }
